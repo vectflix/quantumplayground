d0a644e660b19bca6d76a85cc2549da78762b811
vectflix <malcolmotieno69@gmail.com>
2026-03-08 09:12:09 +0300

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,14 @@
+window.VIDEOS_DATA = [
+    // Physics
+    { ytID: "rZ-p-pM8Y_Q" }, { ytID: "kKKM8Y-u7ds" }, { ytID: "2WCT0W8Ljdg" },
+    { ytID: "kxkFaBA6qbc" }, { ytID: "4i1_97_Z4P8" }, { ytID: "TfYCnOvNnFU" },
+    { ytID: "95V8ko59p40" }, { ytID: "mc979OhitAg" }, { ytID: "KWAsz59F8gA" },
+    // Chemistry
+    { ytID: "h6LPAwA938Y" }, { ytID: "0RRVV4Diomg" }, { ytID: "Sk7W2Vgb6yY" },
+    { ytID: "UL1jmJaUkaQ" }, { ytID: "BY9VpZp_BTA" }, { ytID: "7qOFtL3VEBc" },
+    { ytID: "g5wNg_dKsYY" }, { ytID: "ANi709MYn60" }, { ytID: "UloIw7d9Hao" },
+    // Biology
+    { ytID: "URUJD5NEXC8" }, { ytID: "Ptmlvtei8hw" }, { ytID: "eJ9Zjc-jdys" },
+    { ytID: "sQK3Yr4Sc_k" }, { ytID: "5qSrmeiWsuc" }, { ytID: "gG7uCskUOrA" },
+    { ytID: "Mehz7tCxjSE" }, { ytID: "hOfRN0KihOU" }, { ytID: "hLq2datPo5M" }
+];
