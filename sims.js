0b60610d7d5d502a0fe2e7cef459a1facc02a65b
vectflix <malcolmotieno69@gmail.com>
2026-03-08 09:21:58 +0300

    Create sims.js

---
+++ sims.js
@@ -0,0 +1,34 @@
+window.SIMS_DATA = [
+    // Physics Sims
+    { url: "https://phet.colorado.edu/sims/html/moving-man/latest/moving-man_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/forces-and-motion-basics/latest/forces-and-motion-basics_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/energy-skate-park/latest/energy-skate-park_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/gravity-and-orbits/latest/gravity-and-orbits_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/gas-properties/latest/gas-properties_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/waves-intro/latest/waves-intro_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/bending-light/latest/bending-light_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/circuit-construction-kit-dc/latest/circuit-construction-kit-dc_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/photoelectric-effect/latest/photoelectric-effect_all.html" },
+    
+    // Chemistry Sims
+    { url: "https://phet.colorado.edu/sims/html/build-an-atom/latest/build-an-atom_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/periodic-trends/latest/periodic-trends_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/molecule-shapes/latest/molecule-shapes_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/reactants-products-and-leftovers/latest/reactants-products-and-leftovers_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/states-of-matter/latest/states-of-matter_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/reactions-and-rates/latest/reactions-and-rates_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/reversible-reactions/latest/reversible-reactions_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/ph-scale/latest/ph-scale_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/molarity/latest/molarity_all.html" },
+
+    // Biology Sims
+    { url: "https://phet.colorado.edu/sims/html/membrane-channels/latest/membrane-channels_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/diffusion/latest/diffusion_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/cell-respiration/latest/cell-respiration_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/color-vision/latest/color-vision_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/gene-expression-essentials/latest/gene-expression-essentials_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/gene-expression-basics/latest/gene-expression-basics_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/gene-machine-lac-operon/latest/gene-machine-lac-operon_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/natural-selection/latest/natural-selection_all.html" },
+    { url: "https://phet.colorado.edu/sims/html/energy-forms-and-changes/latest/energy-forms-and-changes_all.html" }
+];
